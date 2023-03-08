314921b31faa820dbd32aa2cfef8c8ba250457f3
kazsuner <mcpabliku@gmail.com>
2023-03-08 13:17:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,47 @@
-import pyshorteners as shr
+from tkinter import * 
 import tkinter as tk
+from datetime import datetime
 from tkinter import messagebox
-import pyperclip
+import pyshorteners
 
 
-class URLShortenerGUI:
-    def __init__(self, master):
-        self.master = master
-        master.title("URL Shortener")
-        master.resizable(False, False)
-        master.configure(background="#FFFFFF")
-        
-        self.label = tk.Label(master, text="Enter URL:", font=("Arial", 12), bg="#FFFFFF")
-        self.label.grid(row=0, column=0, padx=10, pady=10, sticky="w")
-        
-        self.entry = tk.Entry(master, font=("Arial", 12), width=40, bd=2, relief="solid")
-        self.entry.grid(row=0, column=1, padx=10, pady=10, sticky="we")
-        
-        self.button = tk.Button(master, text="Shorten URL", font=("Arial", 12), command=self.shorten_url, bg="#007AFF", fg="#FFFFFF")
-        self.button.grid(row=1, column=1, padx=10, pady=10, sticky="e")
-        
-    def shorten_url(self):
-        url = self.entry.get()
-        shortener = shr.Shortener()
-        short_url = shortener.tinyurl.short(url)
-        
-        # Crea una nueva ventana de mensaje utilizando Toplevel
-        message_box = tk.Toplevel(self.master)
-        message_box.title("Shortened URL")
-        message_box.configure(background="#FFFFFF")
-        
-        message_text = f"The shortened URL is:\n{short_url}"
-        message_label = tk.Label(message_box, text=message_text, font=("Arial", 12), bg="#FFFFFF")
-        message_label.pack(padx=10, pady=10)
-        
-        # Agrega el botón "Copy URL" a la ventana de mensaje
-        copy_button = tk.Button(message_box, text="Copy URL", font=("Arial", 12), command=lambda: self.copy_url(short_url, message_box), bg="#007AFF", fg="#FFFFFF")
-        copy_button.pack(side="bottom", padx=10, pady=10)
-        
-    def copy_url(self, url, message_box):
-        pyperclip.copy(url) 
-        message_box.destroy()
-        self.master.destroy()
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = URLShortenerGUI(root)
-    root.mainloop()
+class url_shortner:
+
+       
+        def create(self):
+                if self.url.get() == "":
+                        messagebox.showerror("Error","Please Paste an URL")
+                else:
+                        self.urls = self.url.get()
+                        self.s = pyshorteners.Shortener()
+                        self.short_url = self.s.tinyurl.short(self.urls)
+                        
+                        self.output = Entry(self.root,font=('verdana',10,'bold'),fg="red",width=30,relief=GROOVE,borderwidth=2,border=2)
+                        self.output.insert(END,self.short_url)
+                        self.output.place(x=80,y=120)
+
+        def __init__(self):
+                self.root = tk.Tk()
+                self.root.geometry('500x200')
+                self.root.maxsize(500,200)
+                self.root.minsize(500,200)
+                self.root.title('Url Shortner')
+                self.root['bg'] = "white"
+
+                self.title = Label(self.root,text="URL Shortner",font=('verdana',15,'bold'),bg="white",fg="green")
+                self.title.place(x=180,y=5)
+
+                Label(self.root,text="Paste Your Url Here ..",font=('verdana',10,'bold'),fg="green").place(x=50,y=50)
+
+                self.url = Entry(self.root,width=50,bg="lightgrey",relief=GROOVE,borderwidth=2,border=2)
+                self.url.place(x=50,y=80)
+
+                self.button = Button(self.root,relief=GROOVE,text="Create",font=('verdana',8,'bold'),bg="green",fg="white",command=self.create)
+                self.button.place(x=360,y=78)
+                self.root.mainloop()
+
+if __name__ == '__main__':
+        url_shortner()
+        
+    
